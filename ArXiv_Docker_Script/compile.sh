46cd5c904256018bd675e1a28a1a4c6e16d54791
Yash Akhauri <akhauri.yash@gmail.com>
2020-05-21 21:32:50 +0530

    minor

--- ArXiv_Docker_Script/compile.sh
+++ ArXiv_Docker_Script/compile.sh
@@ -3,15 +3,16 @@
 # $1 --> Bib file name
 # $2 --> Latex file name
 
-BIBFILE='./$1'
+BIBFILE="./$1"
 
-LATEXFILE='./$2'
+LATEXFILE="./$2"
 
+rm -f *.auxlock *.loa *.unq *.run.xml *.bbl *.blg *.log *.dvi *.fdb_latexmk *.aux *.fls *.lof *.bcf 
 
-docker run --rm --volume $PWD:/workdir --workdir /workdir makisyu/texlive-2016 latexmk $LATEXFILE
+docker run --rm --volume $PWD:/workdir --workdir /workdir makisyu/texlive-2016 pdflatex $LATEXFILE
 
 docker run --rm --volume $PWD:/workdir --workdir /workdir makisyu/texlive-2016 biber $BIBFILE
 
-docker run --rm --volume $PWD:/workdir --workdir /workdir makisyu/texlive-2016 latexmk $LATEXFILE
+docker run --rm --volume $PWD:/workdir --workdir /workdir makisyu/texlive-2016 pdflatex $LATEXFILE
 
-docker run --rm --volume $PWD:/workdir --workdir /workdir makisyu/texlive-2016 latexmk $LATEXFILE
+docker run --rm --volume $PWD:/workdir --workdir /workdir makisyu/texlive-2016 pdflatex $LATEXFILE
